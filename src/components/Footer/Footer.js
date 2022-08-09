34ac02f124d02bf16dace22561967153ac16fed0
Alexey Grabovenko <alexeygrabovenko@gmail.com>
2022-08-09 15:42:10 +0300

    jsx footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,23 @@
 import React from "react";
+import "./Footer.css";
 
 function Footer(props) {
-  return <div>Footer</div>;
+  const date = new Date();
+  return (
+    <section className="footer">
+      <p className="footer__text">
+        Учебный проект Яндекс.Практикум х BeatFilm.
+      </p>
+      <div className="footer__menu">
+        <p className="footer__date">&copy; {date.getFullYear()}</p>
+        <nav classname="footer__links">
+          <a className="footer__link">Яндекс.Практикум</a>
+          <a className="footer__link">Github</a>
+          <a className="footer__link">Facebook</a>
+        </nav>
+      </div>
+    </section>
+  );
 }
 
 export default Footer;
